8250b75888ac21c52adc7c15546861bbf794f92a
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-28 14:53:18 +0530

    add validation to name feild

--- src/main/java/com/bridgelab/AddressBookAppController/AddressBookController.java
+++ src/main/java/com/bridgelab/AddressBookAppController/AddressBookController.java
@@ -2,6 +2,8 @@ package com.blz.AddressBookApp.Controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -43,7 +45,7 @@ public class AddressBookController {
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO> addAddressBookData(@RequestBody AddressBookDTO addressBookDTO) {
+	public ResponseEntity<ResponseDTO> addAddressBookData(@Valid @RequestBody AddressBookDTO addressBookDTO) {
 		AddressBookData addressBookData = null;
 		addressBookData = addressBookService.createAddressBookData(addressBookDTO);
 		ResponseDTO responseDTO = new ResponseDTO("Created Address Book Data Successfully:", addressBookData);
@@ -51,7 +53,7 @@ public class AddressBookController {
 	}
 
 	@PutMapping("/update/{personId}")
-	public ResponseEntity<ResponseDTO> updateAddressBookData(@PathVariable("personId") int personId,@RequestBody AddressBookDTO addressBookDTO) {
+	public ResponseEntity<ResponseDTO> updateAddressBookData(@PathVariable("personId") int personId,@Valid @RequestBody AddressBookDTO addressBookDTO) {
 		AddressBookData addressBookData = null;
 		addressBookData = addressBookService.updateAddressBookData(personId,addressBookDTO);
 		ResponseDTO responseDTO = new ResponseDTO("Updated  Address Book Data Successfully:", addressBookData);
--- src/main/java/com/bridgelab/AddressBookAppDTO/AddressBookDTO.java
+++ src/main/java/com/bridgelab/AddressBookAppDTO/AddressBookDTO.java
@@ -1,8 +1,16 @@
 package com.blz.AddressBookApp.DTO;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class AddressBookDTO {
 
+	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$", message="Address Book Name is Invalid")
+	@NotEmpty(message="Address Book name cannot be null ")
 	public String name;
+	
+	@Pattern(regexp = "^[A-Za-z,.0-9]{3,}$",message = "Address is Invalid!!! ")
+	@NotEmpty(message = "Address cannot be null")
 	public String address;
 
 	public AddressBookDTO(String name, String address) {
