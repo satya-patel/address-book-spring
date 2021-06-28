243cbae84cfd539e6e33ec71537ec98a3cdd71e4
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-28 14:57:18 +0530

    user freindly error response if validation fails

---
+++ src/main/java/com/bridgelab/AddressBookException/AddressBookExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.blz.AddressBookApp.Exception;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.blz.AddressBookApp.DTO.ResponseDTO;
+
+@ControllerAdvice
+public class AddressBookExceptionHandler {
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+		List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+		List<String> errMessage = errorList.stream().map(mapper -> mapper.getDefaultMessage())
+				.collect(Collectors.toList());
+		ResponseDTO responseDTO = new ResponseDTO("Exception while processing rest request", errMessage);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+	}
+}
